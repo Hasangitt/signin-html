b9149d4ad9e5d1e423f948caf06f2838d6314377
Hasangitt <pingpong038759@gmail.com>
2024-09-07 01:00:57 +0600

    signup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
+  theme: {
+    extend: {
+      animation: {
+        'fade-in': 'fadeIn 2s ease-in-out',
+      },
+      keyframes: {
+        fadeOut: {
+          '100%': { opacity: '0' },
+          '0%': { opacity: '1' },
+        }
+      }
+    }
   }
+}
